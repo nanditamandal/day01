82c7aa7a6c762643c278b47b8783cd63ec79a039
kmhasanuiucse <khasan142035@bscse.uiu.ac.bd>
2019-09-12 15:57:52 +0600

    firstprogram

---
+++ myfirstproject/home.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace myfirstproject
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showbutton1 = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.nametextBox1 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // showbutton1
+            // 
+            this.showbutton1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.showbutton1.Location = new System.Drawing.Point(646, 147);
+            this.showbutton1.Name = "showbutton1";
+            this.showbutton1.Size = new System.Drawing.Size(75, 23);
+            this.showbutton1.TabIndex = 0;
+            this.showbutton1.Text = "show";
+            this.showbutton1.UseVisualStyleBackColor = true;
+            this.showbutton1.Click += new System.EventHandler(this.showbutton1_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(63, 157);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(33, 13);
+            this.name.TabIndex = 1;
+            this.name.Text = "name";
+            // 
+            // nametextBox1
+            // 
+            this.nametextBox1.Location = new System.Drawing.Point(271, 150);
+            this.nametextBox1.Name = "nametextBox1";
+            this.nametextBox1.Size = new System.Drawing.Size(295, 20);
+            this.nametextBox1.TabIndex = 2;
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Location = new System.Drawing.Point(271, 268);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(0, 13);
+            this.output.TabIndex = 3;
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.nametextBox1);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.showbutton1);
+            this.Name = "home";
+            this.Text = "home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showbutton1;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox nametextBox1;
+        private System.Windows.Forms.Label output;
+    }
+}
+
